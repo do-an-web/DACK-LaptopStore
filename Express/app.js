cc1c3c2e633a952a5fb673e3337f7e5405647526
1412128@student.hcmus.edu.vn <1412128@student.hcmus.edu.vn>
2018-05-30 09:32:59 +0700

    mergar

--- Express/app.js
+++ Express/app.js
@@ -13,20 +13,15 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-<<<<<<< HEAD
-app.set('view engine', 'hbs'); 
-=======
+app.set('view engine', 'hbs');
 app.engine('hbs', exphbs());
 app.set('view engine', 'hbs');
->>>>>>> db21791c11b8451605e483d3a5495ef883932735
-
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-<<<<<<< HEAD
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
@@ -46,14 +41,14 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-=======
 app.get('/', (req, res) => {
   res.redirect('/home');
 });
 
 app.use('/home', homeController);
 app.use('/shop', shopController);
->>>>>>> db21791c11b8451605e483d3a5495ef883932735
+app.use('/shop', shopController);
+
 
 app.listen(3000, () => {
   console.log('Site running on port 3000');

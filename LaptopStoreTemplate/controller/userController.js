4aa130badd87329bb939169796732f8a465aca6c
1412528 <1412528@student.hcmus.edu.vn>
2018-06-26 17:41:44 +0700

    fix ui account user

--- LaptopStoreTemplate/controller/userController.js
+++ LaptopStoreTemplate/controller/userController.js
@@ -7,40 +7,13 @@ var userRepo = require('../repos/userRepo');
 var restrict = require('../middle-wares/restrict');
 var router = express.Router();
 
-/*router.get('/login', (req, res) => {
-    res.render('user/login');
-});
-*/
-
 /* GET home page. */
 router.get('/signin', (req, res) => {
     var vm = {
-
-       // layout: 'user.layout.hbs',
-
+        //layout: 'user.layout.hbs',
         title: "Sign In"
     };
-    res.render('user/signin',vm);
-});
-
-router.get('/register', (req, res) => {
-     var vm = {
-       // layout: 'user.layout.hbs',
-        title: "Register"
-    };
-    res.render('user/register',vm);
-});
-
-router.get('/profile', restrict,(req, res) => {
-    
-    var date = new Date(res.locals.layoutVM.curUser.f_DOB);
-    date = date.getDate()+'/' + (date.getMonth()+1) + '/'+date.getFullYear();
-    var vm = {
-       // layout: 'user.layout.hbs',
-        title: "Profile",
-        birth: date
-    };
-    res.render('user/profile', vm);
+    res.render('_pageUser/SignIn/index',vm);
 });
 
 router.post('/signin', (req, res) => {
@@ -51,37 +24,38 @@ router.post('/signin', (req, res) => {
 
     userRepo.login(user).then(rows => {
         if (rows.length > 0) {
-            // user = rows[0];
-
             req.session.isLogged = true;
             req.session.user = rows[0];
             req.session.cart = [];
 
-            var url = '/user/profile';
+            var url = '/';
             if (req.query.retUrl) {
                 url = req.query.retUrl;
             }
             res.redirect(url);
-
         } else {
             var vm = {
                 title: "Sign In",
                 showError: true,
                 errorMsg: 'Login failed'
             };
-            res.render('user/signin', vm);
+            res.render('_pageUser/SignIn/index', vm);
         }
     });
 });
-/*Dk tai khoang*/
+
+router.get('/register', (req, res) => {
+    var vm = {
+        title: "Register"
+    };
+    
+    res.render('_pageUser/SignUp/index',vm);
+});
+
 router.post('/register', (req, res) => {
 
     var dob = moment(req.body.dob, 'D/M/YYYY')
         .format('YYYY-MM-DD');
-
-        /*var dob = moment(req.body.dob, 'D/M/YYYY')
-        .format('D/M/YYYY');*/
-
     
     var user = {
         username: req.body.username,
@@ -93,16 +67,44 @@ router.post('/register', (req, res) => {
     };
 
     userRepo.add(user).then(value => {
-        res.render('/user/register');
+        var vm = {
+            title: "Register"
+        };
+        
+        res.render("_pageUser/SignUp/index",vm);
     });
 });
-router.post('/update', (req, res) => {
+
+router.post('/logout', (req, res) => {
     req.session.isLogged = false;
     req.session.user = null;
     // req.session.cart = [];
     
     res.redirect(req.headers.referer);
 });
+// router.post('/update', (req, res) => {
+//     req.session.isLogged = false;
+//     req.session.user = null;
+//     // req.session.cart = [];
+    
+//     res.redirect(req.headers.referer);
+// });
+
+router.get('/profile', restrict,(req, res) => {
+    
+    var date = new Date(res.locals.layoutVM.curUser.f_DOB);
+    date = date.getDate()+'/' + (date.getMonth()+1) + '/'+date.getFullYear();
+    var vm = {
+        title: "Profile",
+        birth: date
+    };
+    res.render('_pageUser/InfoAccount/index', vm);
+});
+
+
+
+
+
 
 router.post('/edit', (req, res) => {
 
@@ -116,29 +118,15 @@ router.post('/edit', (req, res) => {
         email: req.body.email
     };
 
-    
-        res.locals.layoutVM.curUser.f_Name = user.fullname;
-        res.locals.layoutVM.curUser.f_Email = user.email;
-
-   /*var mdob1 = moment(mdob, 'D/M/YYY')
-        .format('YYYY-MM-DD');*/
-
-
-        res.locals.layoutVM.curUser.f_DOB = mdob;
+    res.locals.layoutVM.curUser.f_Name = user.fullname;
+    res.locals.layoutVM.curUser.f_Email = user.email;
+    res.locals.layoutVM.curUser.f_DOB = mdob;
 
     userRepo.update(user).then(rows => {
-       
-
         res.redirect('/user/profile');
     });
 });
 
-router.post('/logout', (req, res) => {
-    req.session.isLogged = false;
-    req.session.user = null;
-    // req.session.cart = [];
-    
-    res.redirect(req.headers.referer);
-});
+
 
 module.exports = router;
